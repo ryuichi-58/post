1e1eb9f69580b7ae40b78ce850ad1a9b9ac814fc
ryuichi-58 <ichi.aka.estab.ot@gmail.com>
2020-12-27 00:15:06 +0900

    会員登録画面追記

---
+++ index.php
@@ -0,0 +1,14 @@
+<p>次のフォームに必要事項をご記入ください。</p>
+<form action="" method="post" enctype="multipart/form-data">
+<dl>
+    <dt>ニックネーム<span class="required">必須</span></dt>
+    <dd><input type="text" name="name" size="35" maxlength="255" /></dd>
+    <dt>メールアドレス<span class="required">必須</span></dt>
+    <dd><input type="text" name="email" size="35" maxlength="255" /></dd>
+    <dt>パスワード<span class="required">必須</span></dt>
+    <dd><input type="text" name="password" size="10" maxlength="20" /></dd>
+    <dt> 写真など</dt>
+    <dd><input type="text" name="image" size="35" /></dd>
+</dl>
+<div><input type="submit" value="入力内容を確認する" /></div>
+</form>
